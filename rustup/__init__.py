ceb04b83d586170f7c45e7d08d2eeaaab3ac8420
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-05-26 13:14:30 -0400

    Add all missing shims

--- rustup/__init__.py
+++ rustup/__init__.py
@@ -8,14 +8,50 @@ from pathlib import Path
 def path() -> Path:
     return Path(shutil.which("rustup"))
 
-def _make_shim(proxy: str) -> None:
+# Note: This function replaces the current process on unix
+#   and spawns a new one on Windows, there's no going back
+#   to the current Python interpreter process!
+def _shim(proxy: str) -> None:
     os.execv(path(), (proxy, *sys.argv[1:]))
 
 def init() -> None:
-    _make_shim("rustup-init")
+    _shim("rustup-init")
 
 def cargo() -> None:
-    _make_shim("cargo")
+    _shim("cargo")
+
+def cargo_clippy() -> None:
+    _shim("cargo-clippy")
+
+def cargo_fmt() -> None:
+    _shim("cargo-fmt")
+
+def cargo_miri() -> None:
+    _shim("cargo-miri")
+
+def clippy_driver() -> None:
+    _shim("clippy-driver")
+
+def rls() -> None:
+    _shim("rls")
+
+def rust_analyzer() -> None:
+    _shim("rust-analyzer")
+
+def rust_gdb() -> None:
+    _shim("rust-gdb")
+
+def rust_gdbgui() -> None:
+    _shim("rust-gdbgui")
+
+def rust_lldb() -> None:
+    _shim("rust-lldb")
 
 def rustc() -> None:
-    _make_shim("rustc")
+    _shim("rustc")
+
+def rustdoc() -> None:
+    _shim("rustdoc")
+
+def rustfmt() -> None:
+    _shim("rustfmt")
